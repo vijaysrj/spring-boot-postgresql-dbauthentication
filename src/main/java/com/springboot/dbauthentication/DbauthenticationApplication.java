a30843fda7b050c515821203b7284a22b3d856e0
unknown <vijay.srj@gmail.com>
2020-06-04 21:51:16 +0530

    db authentication using postgresql

---
+++ src/main/java/com/springboot/dbauthentication/DbauthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.dbauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbauthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbauthenticationApplication.class, args);
+	}
+
+}
